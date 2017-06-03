fd01703b29a4aea97d11300b811683c2c96a429d
Chakradhar Rao Jonagam <9chakri+ds@gmail.com>
2017-06-03 11:10:44 -0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict';
 const fs = require('fs')
 const https = require('https')
-const io = require('socket.io')(8080);
+const io = require('socket.io')(8081);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const K8S_HOST = process.env['K8S_HOST'] || '127.0.0.1'
 const K8S_PORT = process.env['K8S_PORT'] || '8443'
 const K8S_SECRET = process.env['K8S_SECRET'] ||
   fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf-8')
+
+const services=["welcome","time","ks","mysql","myapp","blue","bluegreen","green","scm-web-hooks"]
+const routes=["welcome","time","ks","dbtest","myapp","bluegreen","scm-web-hooks"]
+const projects=["mycliproject","myjbossapp","consoleproject","binarydeploy","bluegreen","scm-web-hooks"];
 
 var global_namespaces=[];
 var socket=null;
@@ -14,6 +18,54 @@ var scores={};
 io.on('connection', function(soc){
   io.emit('message',scores);
 });
+
+
+
+makeReq('/api/v1/services?watch=true',function(rawEvent){
+  try{
+  const event = JSON.parse(rawEvent);
+  console.log(event);
+  }
+  catch(ex){
+    console.log(ex);
+  }
+});
+
+
+makeReq('/api/v1/namespaces?watch=true',function(rawEvent){
+  try{
+  const event = JSON.parse(rawEvent);
+  if(event.type=='ADDED'){
+    global_namespaces[event.object.metadata.name]=event.object.metadata.annotations['openshift.io/requester'];
+  }
+  console.log(global_namespaces);
+  }
+  catch(ex){
+
+  }
+});
+
+makeReq('/oapi/v1/routes?watch=true',function(rawEvent){
+  const event = JSON.parse(rawEvent);
+  var obj={svc:event.object.metadata.name,route:event.object.spec.host,usr:getOwner(event.object.metadata.namespace),type:event.type};
+  var tmp = obj.usr;
+  if(undefined==scores[tmp]){
+    if(typeof(tmp)=='string' && isNaN(tmp)) 
+    scores[tmp]={};
+  }
+  if(obj.type=='ADDED' && undefined!=scores[tmp]){
+    scores[tmp][obj.svc]=1; 
+    console.log(scores);
+    io.emit('message',scores);
+  }
+});
+
+function getOwner (project) {
+  if(undefined!=global_namespaces[project])
+    return global_namespaces[project];
+  else
+    return project;
+}
 
 function makeReq(url,cb){
 
@@ -56,42 +108,4 @@ function makeReq(url,cb){
     });
 
     req.end()
-}
-
-
-
-makeReq('/api/v1/namespaces?watch=true',function(rawEvent){
-  try{
-  const event = JSON.parse(rawEvent);
-  if(event.type=='ADDED'){
-    global_namespaces[event.object.metadata.name]=event.object.metadata.annotations['openshift.io/requester'];
-  }
-  console.log(global_namespaces);
-  }
-  catch(ex){
-
-  }
-});
-
-
-makeReq('/oapi/v1/routes?watch=true',function(rawEvent){
-  const event = JSON.parse(rawEvent);
-  var obj={svc:event.object.metadata.name,route:event.object.spec.host,usr:getOwner(event.object.metadata.namespace),type:event.type};
-  var tmp = obj.usr;
-  if(undefined==scores[tmp]){
-    if(typeof(tmp)=='string' && isNaN(tmp)) 
-    scores[tmp]={};
-  }
-  if(obj.type=='ADDED' && undefined!=scores[tmp]){
-    scores[tmp][obj.svc]=1; 
-    console.log(scores);
-    io.emit('message',scores);
-  }
-});
-
-function getOwner (project) {
-  if(undefined!=global_namespaces[project])
-    return global_namespaces[project];
-  else
-    return project;
 }
